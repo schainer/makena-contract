3fd7726213ba2496c65dc289198a61c2eab871a3
kjunha <kjunha77@gmail.com>
2021-10-06 23:01:48 +0900

    add contracts

---
+++ contracts/MakenaGov.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.0;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Roles.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./MakenaNFT.sol";
+
+
+contract MakenaGov {
+    using Strings for string;
+    using SafeMath for uint256;
+}
---
+++ contracts/MakenaNFT.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Roles.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
+contract MakenaNFT is ERC721Full {
+    using Strings for string;
+    using SafeMath for uint256;
+
+
+}
