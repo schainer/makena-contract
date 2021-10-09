a7e115a4cafc443b548a5e39a1dcef9b91271e84
kjunha <kjunha77@gmail.com>
2021-10-10 00:39:12 +0900

    add testing

--- contracts/MakenaGov.sol
+++ contracts/MakenaGov.sol
@@ -16,14 +16,17 @@ contract MakenaGov is Ownable {
         uint256 diceNumber;
     }
 
+    event Roll(uint256 key);
+    event CheckDice(uint256 dice, uint256 blockNum, bytes32 hash);
+
     mapping(uint256 => Draw) diceNumbers;
     uint256 diceKey = 0;
 
     function roll() public onlyOwner returns (uint256) {
         diceKey += 1;
         diceNumbers[diceKey].blockNumber = block.number + 1;
-        uint256 _key = diceKey;
-        return _key;
+        emit Roll(diceKey);
+        return diceKey;
     }
 
     function checkDice(uint256 key) public onlyOwner returns (uint256 dice, uint256 blockNum, bytes32 hash) {
@@ -33,6 +36,7 @@ contract MakenaGov is Ownable {
             uint256 _diceNum = uint(_blockHash) % 6 + 1;
             diceNumbers[key].diceNumber = _diceNum;
         }
+        emit CheckDice(diceNumbers[key].diceNumber, diceNumbers[key].blockNumber, blockhash(diceNumbers[key].blockNumber));
         dice = diceNumbers[key].diceNumber;
         blockNum = diceNumbers[key].blockNumber;
         hash = blockhash(diceNumbers[key].blockNumber);
---
+++ test/1_gov.test.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+require('chai').use(require('chai-as-promised')).should();
+
+const Gov = artifacts.require('./MakenaGov.sol');
+
+contract('MakenaGov', (accounts) => {
+  let contract;
+  before(async () => {
+    contract = await Gov.new();
+  });
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = contract.address;
+      console.log(address);
+      assert.notEqual(address, '');
+      assert.notEqual(address, 0x0);
+    });
+  });
+  describe('dice', async () => {
+    it('roll dice', async () => {
+      let key = await contract.roll();
+      await contract.roll();
+      await contract.roll();
+      let res = key.logs[0].args.key.toNumber();
+      console.log({ res });
+      let check = await contract.checkDice(res);
+      const { dice, blockNum, hash } = check.logs[0].args;
+      console.log({ dice: dice.toNumber(), blockNum: blockNum.toNumber(), hash });
+    });
+  });
+});
