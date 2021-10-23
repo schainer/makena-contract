fca4872bad7066e027c4f85adc2bd54f09e6a246
kjunha <kjunha77@gmail.com>
2021-10-23 23:57:57 +0900

    add structure to nft contrat

--- contracts/ColorNFT.sol
+++ contracts/ColorNFT.sol
@@ -4,8 +4,52 @@ import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Roles.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/lifecycle/Pausable.sol";
 import "./Strings.sol";
 
-contract ColorNFT {
+// Opensea Proxy Setup
+contract OwnableDelegateProxy {}
+
+contract ProxyRegistry {
+    mapping(address => OwnableDelegateProxy) public proxies;
+}
+
+contract ColorNFT is ERC721Full, Ownable, Pausable {
+
+    address proxyRegistryAddress;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _proxyRegistryAddress
+    ) ERC721(_name, _symbol) {
+        proxyRegistryAddress = _proxyRegistryAddress;
+    }
+
+    function mint() public payable whenNotPaused {
+
+    }
+
+    function setMintPrice() public onlyOwner {
+
+    }
+
+    function getBalance() public view onlyOwner {
+
+    }
+
+    function collect() public onlyOwner {
+
+    }
+
+    function isApprovedForAll(address owner, address operator) public view returns (bool) {
+        // Whitelist OpenSea proxy contract for easy trading.
+        ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+        if (address(proxyRegistry.proxies(owner)) == operator) {
+            return true;
+        }
+        return super.isApprovedForAll(owner, operator);
+    }
+
 
 }
