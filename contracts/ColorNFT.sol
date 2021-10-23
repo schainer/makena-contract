b2b9351c68cac55293acbe6a191cd112fdf119f5
kjunha <kjunha77@gmail.com>
2021-10-23 23:09:36 +0900

    add color nft

---
+++ contracts/ColorNFT.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Roles.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "./Strings.sol";
+
+contract ColorNFT {
+
+}
